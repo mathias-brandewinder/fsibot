49459bf204338c01f8548fa37e766db044d6fc6d
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-08-21 18:24:43 -0700

    Initial worker role
    Upgraded to F# 3.1.0
    Referenced fsharp.compiler.services and LinqToTwitter

---
+++ FsiBot/BotWorkerRole/WorkerRole.fs
@@ -0,0 +1,35 @@
+﻿namespace BotWorkerRole
+
+open System
+open System.Collections.Generic
+open System.Diagnostics
+open System.Linq
+open System.Net
+open System.Threading
+open Microsoft.WindowsAzure
+open Microsoft.WindowsAzure.Diagnostics
+open Microsoft.WindowsAzure.ServiceRuntime
+
+type WorkerRole() =
+    inherit RoleEntryPoint() 
+
+    // This is a sample worker implementation. Replace with your logic.
+
+    let log message (kind : string) = Trace.TraceInformation(message, kind)
+
+    override wr.Run() =
+
+        log "BotWorkerRole entry point called" "Information"
+        while(true) do 
+            Thread.Sleep(10000)
+            log "Working" "Information"
+
+    override wr.OnStart() = 
+
+        // Set the maximum number of concurrent connections 
+        ServicePointManager.DefaultConnectionLimit <- 12
+       
+        // For information on handling configuration changes
+        // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+        base.OnStart()
