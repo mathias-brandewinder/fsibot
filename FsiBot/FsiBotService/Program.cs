0b20f46c4b9307f58c9dba5f15c7f4ca43071e55
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-08-24 20:43:43 -0700

    Windows service version
    Thank you TopShelf for saving my bacon.

---
+++ FsiBot/FsiBotService/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FsiBot;
+using Topshelf;
+
+namespace FsiBotService
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostFactory.Run(x =>                                 
+                    {
+                        x.Service<Bot>(s =>                        
+                            {
+                               s.ConstructUsing(name => new Bot());     
+                               s.WhenStarted(bot => bot.Start());              
+                               s.WhenStopped(bot => bot.Stop());               
+                            });
+
+                        x.RunAsLocalSystem();
+                        x.StartAutomatically();
+                        x.EnableServiceRecovery(s =>
+                            {
+                                s.RestartService(2);
+                                s.RestartComputer(10, "restarting machine.");
+                            });
+
+                        x.SetDescription("FSI bot");
+                        x.SetDisplayName("FSI bot");
+                        x.SetServiceName("fsibot");
+                    });           
+        }
+    }
+}
