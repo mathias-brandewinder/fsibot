1960cc0cdd49694be20705ee2951994633a2ebc7
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-09-06 14:41:01 -0700

    logging incoming messages

--- FsiBot/FsiBotHears/FsiBotHears.fs
+++ FsiBot/FsiBotHears/FsiBotHears.fs
@@ -4,8 +4,16 @@ open System.Threading
 open LinqToTwitter
 open Microsoft.ServiceBus.Messaging
 open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Table
 
-type Message = { StatusId:uint64; User:string; Body:string; }
+//type Message = { StatusId:uint64; User:string; Body:string; }
+
+type LogEntry () =
+    inherit TableEntity ()
+    member val Author = "" with get, set
+    member val Body = "" with get, set
+    member val ID = 0UL with get, set
+    member val Timestamp = System.DateTime.Now with get, set
 
 type Listener () = 
     
@@ -15,11 +23,12 @@ type Listener () =
     let accessTokenSecret = "your access token secret goes here"
     
     let queueConnection = "you connection string goes here"
-    let blobConnection = "you blob connection goes here"
+    let storageConnection = "you blob connection goes here"
 
     let queueName = "mentions"
     let containerName = "lastmention"
     let blobName = "lastID"
+    let logName = "messages"
 
     let pingInterval = 1000 * 60 * 2 // poll every 2 minutes
 
@@ -38,9 +47,16 @@ type Listener () =
         let mentionsQueue = QueueClient.CreateFromConnectionString(queueConnection, queueName)
 
         let container = 
-            let account = CloudStorageAccount.Parse blobConnection
+            let account = CloudStorageAccount.Parse storageConnection
             let client = account.CreateCloudBlobClient ()
             client.GetContainerReference containerName
+
+        let messagesLog = 
+            let account = CloudStorageAccount.Parse storageConnection
+            let client = account.CreateCloudTableClient ()
+            let messagesLog = client.GetTableReference logName
+            messagesLog.CreateIfNotExists () |> ignore
+            messagesLog
 
         let updateLastID (ID:uint64) =
             let lastmention = container.GetBlockBlobReference blobName
@@ -54,6 +70,22 @@ type Listener () =
                 |> System.Convert.ToUInt64
                 |> Some
             else None
+
+        let log (status:Status) =
+            let entry = LogEntry()
+            entry.Author <- status.User.ScreenNameResponse
+            entry.Body <- status.Text
+            entry.ID <- status.StatusID
+            entry.Timestamp <- System.DateTime.UtcNow
+            entry.PartitionKey <- entry.Author
+            entry.RowKey <- entry.ID |> string
+            messagesLog.CreateIfNotExists () |> ignore
+
+            TableOperation.Insert entry
+            |> messagesLog.Execute
+            |> ignore
+
+            status
 
         let queueMention (status:Status) =
             let msg = new BrokeredMessage ()
@@ -80,7 +112,7 @@ type Listener () =
                         select tweet }
                 |> Seq.toList
 
-            mentions |> List.iter queueMention
+            mentions |> List.iter (log >> queueMention)
 
             let updatedSinceID =
                 match mentions with
